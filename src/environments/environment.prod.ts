3f996c04998d83ae49aa36a30b5c3546cedcfebb
unknown <diegoasg5@gmail.com>
2020-10-19 12:25:03 -0500

    generate deploys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuhfRfhRfwdl2g0BJ8hMyqh4ZjQiPZfjg",
+    authDomain: "roomfest-a6feb.firebaseapp.com",
+    databaseURL: "https://roomfest-a6feb.firebaseio.com",
+    projectId: "roomfest-a6feb",
+    storageBucket: "roomfest-a6feb.appspot.com",
+    messagingSenderId: "507009520532",
+    appId: "1:507009520532:web:788c14d83142588ba6b581"
+  }
 };
