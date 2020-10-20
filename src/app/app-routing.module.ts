ff0c23d8b0e1f0747c6d64bdfe12214ac8c12f6a
unknown <diegoasg5@gmail.com>
2020-10-20 11:31:35 -0500

    los deploys de nuevo :v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminPageComponent } from './Pages/admin-page/admin-page.component';
-import { ManageProductsComponent } from './Pages/admin-page/manage-products/manage-products.component';
-import { ManageUsersComponent } from './Pages/admin-page/manage-users/manage-users.component';
-import { MangaEventsComponent } from './Pages/admin-page/manga-events/manga-events.component';
+import { AdminPageComponent } from 'src/app/Pages/admin-page/admin-page.component';
+import { ManageProductsComponent } from 'src/app/Pages/admin-page/manage-products/manage-products.component';
+import { ManageUsersComponent } from 'src/app/Pages/admin-page/manage-users/manage-users.component';
+import { MangaEventsComponent } from 'src/app/Pages/admin-page/manga-events/manga-events.component';
 import { ProductViewComponent } from './Pages/Global/product-view/product-view.component';
-import { GuestComponent } from './Pages/guest/guest.component';
-import { HomepageComponent } from './Pages/Homepage/homepage.component';
-import { PerfilProveedorComponent } from './Pages/proveedor/perfil-proveedor/perfil-proveedor.component';
-import { ProductosComponent } from './Pages/proveedor/productos/productos.component';
-import { ProveedorComponent } from './Pages/proveedor/proveedor.component';
-import { SolicitudesComponent } from './Pages/proveedor/solicitudes/solicitudes.component';
-import { CreateEventComponent } from './Pages/roompage/create-event/create-event.component';
-import { MyEventsComponent } from './Pages/roompage/my-events/my-events.component';
-import { ProfileComponent } from './Pages/roompage/profile/profile.component';
-import { RoompageComponent } from './Pages/roompage/roompage.component';
-import { SignupPageComponent } from './Pages/SignUp/signup-page.component';
+import { GuestComponent } from 'src/app/Pages/guest/guest.component';
+import { HomepageComponent } from 'src/app/Pages/Homepage/homepage.component';
+import { PerfilProveedorComponent } from 'src/app/Pages/proveedor/perfil-proveedor/perfil-proveedor.component';
+import { ProductosComponent } from 'src/app/Pages/proveedor/productos/productos.component';
+import { ProveedorComponent } from 'src/app/Pages/proveedor/proveedor.component';
+import { SolicitudesComponent } from 'src/app/Pages/proveedor/solicitudes/solicitudes.component';
+import { CreateEventComponent } from 'src/app/Pages/roompage/create-event/create-event.component';
+import { MyEventsComponent } from 'src/app/Pages/roompage/my-events/my-events.component';
+import { ProfileComponent } from 'src/app/Pages/roompage/profile/profile.component';
+import { RoompageComponent } from 'src/app/Pages/roompage/roompage.component';
+import { SignupPageComponent } from 'src/app/Pages/SignUp/signup-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'homepage', pathMatch: 'full' },
